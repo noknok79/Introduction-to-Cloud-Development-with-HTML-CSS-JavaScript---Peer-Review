702dffcdb306da8fa9f335ea2c1c938123a92328
noknok79 <nokiev@yahoo.com>
2021-03-02 10:17:41 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,30 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal == "") { //Here Check is empty
+        alert("Amount can't by empty or alphabet character");
+        document.getElementById("principal").focus();
+        return false;
+    }else{
+        if (principal <= 0) { //Here check is a positive number
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
+        }
+    }
+
+
+    var hoy= new Date().getFullYear(); //Mark Anthony Villanueva - Peer Review Project Here get the year
+    var futuro= parseInt(hoy,10) + parseInt(years,10);
+    var interest= principal * years * rate / 100;
+    var result= "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + futuro + "</mark>";
+    document.getElementById("result").innerHTML=result;
+}
+
+function showVal(newVal){ //Show range value in span
+    document.getElementById("showRate").innerHTML=newVal + '%';
+}
+        
